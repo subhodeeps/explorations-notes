af04cd2169ac7dfa6f190890ac070cb9e315591b
subhodeeps <subhodeep.sarkar1@gmail.com>
2025-01-23 12:24:37 +0530

    first commit

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -63,5 +63,23 @@ module.exports = {
         purge: false,
       },
     },
+
+    // fonts
+
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Space Mono`,
+            file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
+          },
+        ],
+      },
+    },
+
   ],
 }
+
